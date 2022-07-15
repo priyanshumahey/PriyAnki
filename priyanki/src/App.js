59afc180ed29a420e51c6e4c3e874837d7de9204
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-07-15 15:32:19 -0700

    Installed Redux

--- priyanki/src/App.js
+++ priyanki/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router,Switch,Route,Redirect} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
 
 //Start Page
 import Start from './Start/Start'
@@ -7,18 +7,18 @@ import Start from './Start/Start'
 //Home Page
 import Homepage from './Homepage/Home'
 
+
 function App() {
   const start = () => {
     return console.log("ButtonWorks")
   }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Welcome to PriyAnki!
         </p>
-        <Start />
-        <Homepage />
         <button onClick={start}>Start</button>
       </header>
     </div>
