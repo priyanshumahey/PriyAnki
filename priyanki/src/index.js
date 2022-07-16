6e65f03a3dd9bdbc99ce133d0cfa0ce60c055e4a
priyanshumahey <72003945+priyanshumahey@users.noreply.github.com>
2022-07-16 08:09:04 -0700

    Switched to better-sqlite3
    Currently experiencing issues with better-sqlite3

--- priyanki/src/index.js
+++ priyanki/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Database from 'better-sqlite3';
 
 //Start Page
 import Start from './Start/Start'
