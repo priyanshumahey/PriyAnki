058a09371646099d9950efb1347fa854ac602612
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2022-07-20 16:18:54 -0700

    Update Start.js

--- priyanki/src/Start/Start.js
+++ priyanki/src/Start/Start.js
@@ -1,9 +1,20 @@
 import "./Start.css"
  
 
+const BoxSetting = () => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
 function Start () {
     return (
+        <>
         <p className="App">Start component!</p>
+        <BoxSetting />
+        <BoxSetting />
+        <BoxSetting />
+        </>
     )
 }
 
